35c7979bca9e75328a19e84d7f312580feb36204
Xuan Hoa PHAM <xuanhoa.pham@shaigo.ca>
2022-02-22 16:46:12 -0500

    spring security auth 2 compiled and ran

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.6.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
+}
+
+group = "com.poc.spring.security"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // spring security oauth2
+    implementation ("org.springframework.security.oauth:spring-security-oauth2:2.4.1.RELEASE")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/poc/spring/security/oauth2/SpringSecurityOauth2ConfidentialWebAppApplication.kt
@@ -0,0 +1,11 @@
+package com.poc.spring.security.oauth2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityOauth2ConfidentialWebAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityOauth2ConfidentialWebAppApplication>(*args)
+}
